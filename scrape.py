301a0aeb34ff321499b1406944a613b5406ec2da
AbigailTaylor <ahart1@gwu.edu>
2017-03-28 11:14:05 -0400

    College Scrape

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html'
+url = 'https://columbian.gwu.edu/2015-2016'
 response = requests.get(url)
 html = response.content
 
@@ -25,7 +25,6 @@ for row in table.findAll('tr')[1:-1]:
             list_of_cells.append(cell.text)
     list_of_rows.append(list_of_cells)
 
-outfile = open("executions.csv", "wb")
+outfile = open("college.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["scheduled_execution", "link", "last_name", "first_name", "TDCJ_Number", "birth_date", "race", "date", "county"])
-writer.writerows(list_of_rows)
+writer.writerow(["academic_year", "department", "faculty", "sponsor", "title_of_project"])
