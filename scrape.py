5c42611cdd510b1f06158fae87af7c4e30e95fac
AbigailTaylor <ahart1@gwu.edu>
2017-03-28 11:21:24 -0400

    Corrected College Scrape

--- scrape.py
+++ scrape.py
@@ -14,17 +14,10 @@ counter = 1
 for row in table.findAll('tr')[1:-1]:
     list_of_cells = []
     for cell in row.findAll('td'):
-        if cell.text == '':
-            list_of_cells.append(counter)
-            counter += 1
-        elif cell.text != '' and cell.text[0] == '(':
-            party, state = cell.text.split(', ')
-            list_of_cells.append(party.replace('(','').replace(')', ''))
-            list_of_cells.append(state)
-        else:
-            list_of_cells.append(cell.text)
+        list_of_cells.append(cell.text)
     list_of_rows.append(list_of_cells)
 
 outfile = open("college.csv", "wb")
 writer = csv.writer(outfile)
 writer.writerow(["academic_year", "department", "faculty", "sponsor", "title_of_project"])
+writer.writerows(list_of_rows)
